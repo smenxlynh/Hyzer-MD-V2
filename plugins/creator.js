c164a3b67ae91ef96bcfe5603fefb136f728fb7d
smenxlynh <105121512+smenxlynh@users.noreply.github.com>
2022-06-01 11:18:44 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -14,7 +14,7 @@ const q = {
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6287892711054', 'Hyzer', m)
+conn.sendContact(m.chat, '60111182951', 'nxlynh', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
